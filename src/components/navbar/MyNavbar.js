8e254b5c207fb5d268df7eb4e5d17f13d0712bb8
majiiddddd <majid.fathi12@gmail.com>
2024-09-15 16:28:19 +0200

    Navbar code

---
+++ src/components/navbar/MyNavbar.js
@@ -0,0 +1,38 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { NavLink } from "react-router-dom";
+import './MyNavbar.css';
+
+function MyNavbar() {
+  const expand = "md";
+  return (
+    <Navbar key={expand} style={{backgroundColor:'#ccc'}} expand={expand} className="mb-3">
+      <Container fluid>
+        <Navbar.Brand href="#">Blog Project</Navbar.Brand>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-${expand}`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+            Blog Project
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <NavLink className='nav-link' to='/'>Home</NavLink>
+              <NavLink className='nav-link' to='/add-article'>create Article</NavLink>
+              <NavLink className='nav-link' to='/about'>About</NavLink>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MyNavbar;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,14 @@
+import MyNavbar from './../../components/navbar/MyNavbar'
+
 function Home(){
     return(
+        <>
+
+        <MyNavbar />
         <h1>Home Page</h1>
+
+        </>
+        
     );
 }
 
