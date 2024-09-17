c6a61b16306b09042a77aa8c9b2a2a975352567a
majiiddddd <majid.fathi12@gmail.com>
2024-09-17 08:42:39 +0200

    addArticle base codes

--- src/pages/addArticle/AddArticle.js
+++ src/pages/addArticle/AddArticle.js
@@ -1,13 +1,45 @@
-import MyNavbar from './../../components/navbar/MyNavbar';
+import MyNavbar from "./../../components/navbar/MyNavbar";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "./AddArticle.css";
 
-function AddArticle(){
-    return(
-        <>
-        <MyNavbar />
-        <h1>AddArticle Page</h1>
-        
-        </>
-    );
+function AddArticle() {
+  return (
+    <>
+      <MyNavbar />
+      <div className="formContainer">
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" placeholder="Type here.." />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Desc</Form.Label>
+            <Form.Control type="text" placeholder="Type here.." />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Author</Form.Label>
+            <Form.Control type="text" placeholder="Type here.." />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Category</Form.Label>
+            <Form.Control type="text" placeholder="Type here.." />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Image</Form.Label>
+            <Form.Control type="text" placeholder="Type here.." />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Reading Time</Form.Label>
+            <Form.Control type="number" placeholder="Type here.." />
+          </Form.Group>
+          <Button variant="primary" type="button">
+            Create Article
+          </Button>
+        </Form>
+      </div>
+    </>
+  );
 }
 
 export default AddArticle;
